1aa164af5058b3784ea7282e10df976606bcd916
Maheer1100 <maheernaqvi2@gmail.com>
2024-09-30 21:56:34 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar'; // Assuming Navbar.js is in the same folder
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <section id="home">
+        {/* Your landing page content goes here */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './Navbar.css'; // Make sure to import the CSS
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  // Detect scroll event
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${scrolled ? 'navbar-scrolled' : ''}`}>
+      <div className="logo">ProDigy Infotech</div>
+      <ul className="menu">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
